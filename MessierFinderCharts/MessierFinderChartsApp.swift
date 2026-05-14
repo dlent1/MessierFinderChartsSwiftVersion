5c37de0621b23744c54abc70f995e0b9e0766b2e
dlent1 <dave_meade@yahoo.com>
2026-05-14 13:04:28 -0500

    Initial Commit

---
+++ MessierFinderCharts/MessierFinderChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessierFinderChartsApp.swift
+//  MessierFinderCharts
+//
+//  Created by David Lent on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct MessierFinderChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
